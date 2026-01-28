8ae97ea04c9d698c08cb09d4e51d7db54908d58f
danielmusselwhite <daniel.l.musselwhite@gmail.com>
2026-01-28 20:05:07 +0000

    add - Authorize view to mainlayout

--- MtgDeckWebsite/Program.cs
+++ MtgDeckWebsite/Program.cs
@@ -14,11 +14,25 @@ builder.Services.AddRazorComponents()
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// todo - Add Identity
-//builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
-//    .AddEntityFrameworkStores<ApplicationDbContext>();
+#region Add Identity
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 
+// todo - delete me
+// Add Identity Razoor Pages
+//builder.Services.AddRazorPages();
+//builder.Services.AddServerSideBlazor();
+#endregion
+
+// Build the app
 var app = builder.Build();
+
+#region Blazor and Identity Endpoints
+// todo - delete me
+//app.MapRazorPages();           // Enable Razor Pages, including Identity pages (Login, Register, Logout)
+//app.MapBlazorHub();            // Enable the SignalR hub for Blazor Server
+//app.MapFallbackToPage("/_Host"); // Fallback for unmatched URLs: serves _Host.cshtml so Blazor routing can handle the request
+#endregion
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
